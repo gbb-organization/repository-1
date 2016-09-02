09de13d46dae05fbb2b0c87ae63324a53dcaf02c
gbb-org-member <gbb-org-member@users.noreply.github.com>
2016-09-02 13:02:09 +0300

    commit1

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
-var i = 0;
-
-process.stdout.write('Running failed test\n');
+process.stdout.write('Running succeed test\n');
 
 setInterval(function () {
     process.stdout.write('.');
 }, 1000);
 
 setTimeout(function () {
-    process.stderr.write('\nTest failed');
-    process.exit(1);
+    /*eslint-disable no-process-exit*/
+    process.stdout.write('\nTest passed');
+    process.exit(0);
+    /*eslint-enable no-process-exit*/
 }, 10000);
